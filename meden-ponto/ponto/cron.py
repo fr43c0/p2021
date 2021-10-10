594645e47d8565edec7aed14723d20759f8681a4
paulo <fr43c0@gmail.com>
2021-10-10 02:59:02 -0300

    arremates 4

--- meden-ponto/ponto/cron.py
+++ meden-ponto/ponto/cron.py
@@ -1,8 +1,8 @@
 from ponto.models import Obs
-
+from django.contrib.auth.models import User
 
 
 def derruba():
-    obs=Obs.objects.create(colaborador='franco',observacoes='fake')
-    obs.save()
-derruba()
+    u=User.objects.all()[0]
+    print(u)
+    obs=Obs.objects.create(colaborador=u,observacoes='fake')
