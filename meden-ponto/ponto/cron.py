d5848620659307320272d7a244c741b4051bf3d2
fr43c0 <fr43c0@gmail.com>
2021-10-12 23:28:23 -0300

    delete cron.py

--- meden-ponto/ponto/cron.py
+++
@@ -1,8 +0,0 @@
-from ponto.models import Obs
-from django.contrib.auth.models import User
-
-
-def derruba():
-    u=User.objects.all()[0]
-    print(u)
-    obs=Obs.objects.create(colaborador=u,observacoes='fake')
